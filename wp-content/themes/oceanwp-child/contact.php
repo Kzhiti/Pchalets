d8c1d0d40b0537f04d57fd91e69ff6a1ab911677
kzhiti <kylianzh@gmail.com>
2020-11-29 12:15:04 +0100

    contact page done

--- wp-content/themes/oceanwp-child/contact.php
+++ wp-content/themes/oceanwp-child/contact.php
@@ -4,6 +4,7 @@
  */
 get_header();
 ?>
+<p class="contact"><br><br>Marie DUBOIS<br>24 Rue du Chalet<br>37789 CHALET<br>06.66.57.35.34<br>marie.dubois@chaletscaviar.fr<br><br><br></p>
 <form class="form_contact" method="post">
     <div><label for="nom">Votre nom:</label><br><input name="nom" type="text"></div>
     <div><label for="mail">Votre e-mail:</label><br><input name="mail" type="email"></div>
